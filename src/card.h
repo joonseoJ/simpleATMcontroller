6123021cad0d567472a35cc3d6fe692266676ce7
joonseoJ <francejs1025@gmail.com>
2022-04-07 15:32:40 +0900

    Refactoring 1. add post _ to member variables

--- src/card.h
+++ src/card.h
@@ -9,8 +9,8 @@ class Card{
 		Card();
 
 		~Card(){
-			cardCompany.clear();
-			userName.clear();
+			cardCompany_.clear();
+			userName_.clear();
 		}
 
 		void clearCompanyAndUserName();
@@ -23,7 +23,7 @@ class Card{
 		std::string& getCardCompany();
 		std::string& getUserName();
 	private:
-		std::string cardCompany, userName;
+		std::string cardCompany_, userName_;
 };
 
 class CardException : public std::exception{
